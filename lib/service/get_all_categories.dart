ed80db8b9e276db9f7ab30da67f8d165c447a71a
omar8157 <mrm548500@gmail.com>
2026-03-07 21:37:27 +0200

    get all categories service

---
+++ lib/service/get_all_categories.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:store_app/constants/service_constants.dart';
+
+class GetAllCategories {
+  final dio = Dio(BaseOptions(baseUrl: ServiceConstants.baseUrl));
+
+  Future<List<dynamic>> getAllCategories() async {
+    final Response response = await dio.get("products/categories");
+    List<dynamic> categories = response.data;
+    return categories;
+  }
+}
