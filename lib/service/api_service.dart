6696cdfc28ad87abf8416bf4e60a4d52436353ab
omar8157 <mrm548500@gmail.com>
2026-03-08 02:39:08 +0200

    feat: implement ApiService for handling API requests

---
+++ lib/service/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:store_app/constants/service_constants.dart';
+
+class ApiService {
+  final Dio dio = Dio(BaseOptions(baseUrl: ServiceConstants.baseUrl));
+
+  Future<dynamic> get({required String url}) async {
+    Response response = await dio.get(url);
+    if (response.statusCode == 200) {
+      final data = response.data;
+      return data;
+    } else {
+      throw Exception(response.statusMessage);
+    }
+  }
+}
